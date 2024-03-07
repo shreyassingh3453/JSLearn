2e6d4a7eb8403caf35cf440bad86e1325c166bdd
Shreyas Singh <53165514+shreyassingh3453@users.noreply.github.com>
2024-03-07 19:36:43 +0530

    strings

--- 01_array.js
+++ 01_array.js
@@ -1,2 +1,10 @@
-const arr=[1,2,3,'Shreyas','Javascript','Python']
-console.log(arr.includes(5))
+const arr = [1, 2, 3, 'Shreyas', 'Javascript', 'Python']
+// console.log(arr.includes('Shreyas'))
+// console.log(arr.indexOf('Shreyas'));
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+
+// }
+const newArr = [4, 5, 6, 7]
+const mergedArr = [...arr, ...newArr] // arr.concat(new_arr)
+console.log(mergedArr)
---
+++ 02_string.js
@@ -0,0 +1,11 @@
+// template literals -> way to emebed expressins in strings using string interpolation
+
+const myname = 'Shreyas'
+console.log(`Hello my name is ${myname} nice to meet you !!`)
+
+// String methdos ->
+let str = myname.toUpperCase()
+console.log(str)
+str = myname.toLowerCase()
+console.log(str);
+
