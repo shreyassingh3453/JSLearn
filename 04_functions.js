fe07b634d625c1bfb5b7c6fc77559908981c5d88
Shreyas Singh <53165514+shreyassingh3453@users.noreply.github.com>
2024-04-15 19:43:30 +0530

    functions

--- 04_functions.js
+++ 04_functions.js
@@ -27,7 +27,7 @@
 // let count = result(value)
 // console.log(count);
 
-//For a given array of nmbers print its square using foreach()
+//QFor a given array of nmbers print its square using foreach()
 
 // let arr = [1, 2, 3, 4, 5, 6];
 
@@ -35,3 +35,29 @@
 //   console.log(val * val);
 // });
 // arr.forEach()
+
+//Q Find largest element using reduce method in array
+// let arr = [32, 41, 1, 101, 6, 1000];
+
+// const result = arr.reduce((prev, curr) => {
+//   return prev > curr ? prev : curr;
+// });
+// console.log(result);
+
+//Q Given an array with marks of students filter out marks which is greater than 90
+
+// let marks = [82, 93, 97, 43, 45, 69, 96, 100, 95, 32];
+
+// const result = marks.filter((val) => {
+//   return val > 90;
+// });
+// console.log(result);
+
+//IIFE O/P QUESTION
+// (function (x) {
+//   return (function (y) {
+//     console.log(x);
+//   })(2);
+// })(1);
+
+//Closures
