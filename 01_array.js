2df822b6378f4a6ba42c7e68aa28a3e56a604129
Shreyas Singh <53165514+shreyassingh3453@users.noreply.github.com>
2024-05-20 16:31:27 +0530

    some Questions added

--- 01_array.js
+++ 01_array.js
@@ -1,12 +1,12 @@
-const prompt = require('prompt-sync')();
-let size = prompt('Enter the size of array')
+// const prompt = require("prompt-sync")();
+// let size = prompt("Enter the size of array");
 // const arr = Array.from({length:size}, ()=> prompt('Enter your elements: '))
-const arr = []
-for (let i = 0; i < size; i++) {
-    let n = prompt(`Enter your element: `)
-    arr.push(n)
-}
-console.log(arr);
+// const arr = ["1", "33", "48", "21", "18", "16", "19", "69", "70", "44"];
+// for (let i = 0; i < size; i++) {
+//   let n = prompt(`Enter your element: `);
+//   arr.push(n);
+// }
+// console.log(arr);
 
 // console.log(arr.includes('Shreyas'))
 // console.log(arr.indexOf('Shreyas'));
@@ -17,3 +17,83 @@ console.log(arr);
 // const newArr = [4, 5, 6, 7]
 // const mergedArr = [...arr, ...newArr] // arr.concat(new_arr)
 // console.log(mergedArr)
+
+//FindIndex()
+// console.log(
+//   arr.findIndex((value) => {
+//     return value > 18;
+//   })
+// );
+// const arr = [21, 100, 10, 45, 17];
+// arr.sort((a, b) => {
+//   let c = a - b;
+//   return c;
+// });
+// console.log(arr);
+//Q1 Find the second largest number in an array
+//Input : [12,35,1,10,34,1,34] --O/P: 34
+// INput : [10,5,10] O/P : 5
+
+// function secondLargest(arr) {
+//   //arr.sort((a,b)=> b-a)
+//   let largest = Number.NEGATIVE_INFINITY;
+//   let secondLargest = Number.NEGATIVE_INFINITY;
+//   if (arr.length <= 1) {
+//     return "Not Applicable";
+//   }
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > largest) {
+//       secondLargest = largest;
+//       largest = arr[i];
+//     } else if (arr[i] != largest && arr[i] > secondLargest) {
+//       secondLargest = arr[i];
+//     }
+//   }
+//   return secondLargest;
+// }
+// console.log(secondLargest([21, 100, 10, 45, 17]));
+// let arr = [21, 100, 10, 45, 17];
+// arr.sort((a, b) => a - b);
+// console.log(arr);
+// let a = BigInt(
+//   "9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999"
+// );
+// let b = BigInt(
+//   "9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999998"
+// );
+// let result = a - b;
+// console.log(result.toString());
+//Q2 Rotate Array by K
+//Given an integer array nums rotate it right by k steps
+//Input : [1,2,3,4,5,6,7] k=3 ----> Output: [7,1,2,3,4,5,6] k=1 - > [6,7,1,2,3,4,5] k=2 -> [5,6,7,1,2,3,4] k=3
+
+// function rotateArray(nums, k) {
+//   let size = nums.length;
+//   if (k > size) {
+//     k = k % size;
+//   }
+//   if (k == size) {
+//     console.log("Resultant array will be the same");
+//     return nums;
+//   }
+//   let temp = nums.splice(size - k, size);
+//   nums.unshift(...temp);
+//   return nums;
+// }
+// console.log(rotateArray([1, 2, 3, 4, 5, 6, 7], 3));
+//Q3 - Remove Duplicates Given an integer array nums sorted in non-decreasing order, your task is to remove the duplicates in-place.
+//Each unique element should appear only once, and the relative order of the elements should be preserved.
+//After removing the duplicates, return the number of unique elements in nums.
+//Input [0,0,1,1,1,2,2,3,3,4] --> output [0,1,2,3,4]
+
+// function removeDuplicates(nums) {
+//   for (let i = 0; i < nums.length - 1; i++) {
+//     if (nums[i] == nums[i + 1]) {
+//       nums.splice(i + 1, 1);
+//       --i;
+//     }
+//   }
+//   return nums;
+// }
+
+// console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
